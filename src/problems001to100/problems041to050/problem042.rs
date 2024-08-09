eb853908026e11aac62719dbbba3502598628e4b
John Butler <john.m.butler.iv@gmail.com>
2024-08-08 22:56:39 -0500

    do 42 and 43

--- src/problems001to100/problems041to050/problem042.rs
+++ src/problems001to100/problems041to050/problem042.rs
@@ -1,28 +1,62 @@
 // https://projecteuler.net/problem=42
 
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::Path,
+};
+
+const WORDS_PATH: &str = "./src/problems001to100/problems041to050/problem042_words.txt";
+
 pub fn make() -> crate::Problem {
     crate::Problem {
-        title: "template",
-        number: 0,
+        title: "Coded Triangle Numbers",
+        number: 42,
         solve: core_solve,
     }
 }
 
-fn core_solve() -> i64 {
-    todo!()
+fn read_words(path: &str) -> Vec<Vec<u8>> {
+    let path = Path::new(path);
+    let file = match File::open(path) {
+        Err(_) => return vec![],
+        Ok(file) => file,
+    };
+
+    let reader = BufReader::new(&file);
+    reader
+        .split(b',')
+        .map(|maybe_bytes| {
+            let bytes = maybe_bytes.unwrap_or_default();
+            bytes[1..(bytes.len() - 1)].to_vec()
+        })
+        .collect()
 }
 
-/*
+fn core_solve() -> i64 {
+    // the longest word is 14 letters long. The maximum letter value is 26
+    let mut is_triangular_cache = [false; 14 * 26 + 1];
+    let mut current_tri = 1;
+    let mut current_tri_index = 1;
+    while current_tri < is_triangular_cache.len() {
+        is_triangular_cache[current_tri] = true;
+        current_tri_index += 1;
+        current_tri += current_tri_index;
+    }
+    let is_triangular_cache = is_triangular_cache;
+
+    read_words(WORDS_PATH)
+        .iter()
+        .map(|bytes| bytes.iter().map(|byte| byte - b'A' + 1).sum::<u8>())
+        .filter(|n| is_triangular_cache[*n as usize])
+        .collect::<Vec<_>>()
+        .len() as i64
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn toy_example() {
-        assert_eq!(super::core_solve(), 0)
-    }
-
-    #[test]
     fn verify_answer() {
-        assert_eq!((super::make().solve)(), 0)
+        assert_eq!((super::make().solve)(), 162)
     }
 }
- */
--- src/problems001to100/problems041to050/problem043.rs
+++ src/problems001to100/problems041to050/problem043.rs
@@ -1,28 +1,51 @@
 // https://projecteuler.net/problem=43
 
+use crate::euler_tools::{
+    collection_tools::{inplace_permute, PermutationStatus},
+    DigitIterator,
+};
+
 pub fn make() -> crate::Problem {
     crate::Problem {
-        title: "template",
-        number: 0,
+        title: "Sub-String Divisibility",
+        number: 43,
         solve: core_solve,
     }
 }
 
-fn core_solve() -> i64 {
-    todo!()
+fn is_substring_divisible(digits: &[i64]) -> bool {
+    // digits are little-endian, so the indexes don't match the problem
+    digits[6] & 1 == 0
+        && (digits[5] + digits[6] + digits[7]) % 3 == 0
+        && (digits[4] == 0 || digits[4] == 5)
+        && DigitIterator::<i64>::combine_digits(&digits[3..=5]) % 7 == 0
+        && DigitIterator::<i64>::combine_digits(&digits[2..=4]) % 11 == 0
+        && DigitIterator::<i64>::combine_digits(&digits[1..=3]) % 13 == 0
+        && DigitIterator::<i64>::combine_digits(&digits[..=2]) % 17 == 0
 }
 
-/*
+fn core_solve() -> i64 {
+    let mut digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let mut sum = 0;
+    while inplace_permute(&mut digits) != PermutationStatus::Finished {
+        if is_substring_divisible(&digits) {
+            sum += DigitIterator::<i64>::combine_digits(&digits);
+        }
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn toy_example() {
-        assert_eq!(super::core_solve(), 0)
+        assert!(super::is_substring_divisible(&[9, 8, 2, 7, 5, 3, 6, 0, 4]))
     }
 
     #[test]
     fn verify_answer() {
-        assert_eq!((super::make().solve)(), 0)
+        assert_eq!((super::make().solve)(), 16695334890)
     }
 }
-*/

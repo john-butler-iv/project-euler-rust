cdd45d6117f0ff07f269bdaa7a4fdc5357ca12be
John Butler <john.m.butler.iv@gmail.com>
2024-08-10 10:55:46 -0500

    do 48

--- src/problems001to100/problems041to050/problem048.rs
+++ src/problems001to100/problems041to050/problem048.rs
@@ -1,29 +1,44 @@
 // https://projecteuler.net/problem=48
 
+use std::ops::Add;
+
+use num_bigint::BigUint;
+use num_traits::One;
+
 pub fn make() -> crate::Problem {
     crate::Problem {
-        title: "template",
-        number: 0,
-        solve: core_solve,
+        title: "Self Powers",
+        number: 48,
+        solve: || core_solve(1000),
     }
 }
 
-fn core_solve() -> i64 {
-    todo!()
+fn core_solve(last_num: u32) -> i64 {
+    let mut sum = BigUint::ZERO;
+
+    let one = BigUint::one();
+    let mut big_n = BigUint::one();
+    for n in 1..=last_num {
+        sum = sum.add(big_n.pow(n));
+
+        assert_eq!(big_n, BigUint::from(n));
+        big_n = big_n.add(&one);
+    }
+
+    let string_sum = sum.to_string();
+    string_sum[string_sum.len() - 10..].parse().unwrap()
 }
 
-/*
 #[cfg(test)]
 mod tests {
     #[test]
+    #[allow(clippy::zero_prefixed_literal)]
     fn toy_example() {
-        assert_eq!(super::core_solve(), 0)
+        assert_eq!(super::core_solve(10), 0405071317)
     }
 
     #[test]
     fn verify_answer() {
-        assert_eq!((super::make().solve)(), 0)
+        assert_eq!((super::make().solve)(), 9110846700)
     }
 }
-
-*/

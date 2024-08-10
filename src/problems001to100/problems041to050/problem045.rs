cfd35a1af78758b497009cd5adf1493eb1f3bf24
John Butler <john.m.butler.iv@gmail.com>
2024-08-09 20:56:40 -0500

    45

--- src/problems001to100/problems041to050/problem045.rs
+++ src/problems001to100/problems041to050/problem045.rs
@@ -1,28 +1,59 @@
 // https://projecteuler.net/problem=45
 
+use std::cmp;
+
+use crate::euler_tools::{Hexagon, Pentagon, Triangle};
+
 pub fn make() -> crate::Problem {
     crate::Problem {
-        title: "template",
-        number: 0,
-        solve: core_solve,
+        title: "Triangular, Pentagonal, and Hexagonal",
+        number: 45,
+        solve: || core_solve(285, 165, 143),
     }
 }
 
-fn core_solve() -> i64 {
-    todo!()
+fn core_solve(starting_t: i64, starting_p: i64, starting_h: i64) -> i64 {
+    let mut t_index = starting_t + 1;
+    let mut triangle = t_index.triangle();
+    let mut p_index = starting_p + 1;
+    let mut pentagon = p_index.pentagon();
+    let mut h_index = starting_h;
+    let mut hexagon = h_index.hexagon();
+
+    while triangle != pentagon {
+        hexagon += 4 * h_index + 1;
+        h_index += 1;
+        loop {
+            match hexagon.cmp(&pentagon) {
+                cmp::Ordering::Less => {
+                    hexagon += 4 * h_index + 1;
+                    h_index += 1;
+                }
+                cmp::Ordering::Greater => {
+                    pentagon += 3 * p_index + 1;
+                    p_index += 1;
+                }
+                cmp::Ordering::Equal => break,
+            }
+        }
+        while triangle < hexagon {
+            t_index += 1;
+            triangle += t_index;
+        }
+    }
+
+    triangle
 }
 
-/*
 #[cfg(test)]
 mod tests {
     #[test]
     fn toy_example() {
-        assert_eq!(super::core_solve(), 0)
+        assert_eq!(super::core_solve(1, 1, 1), 40755)
     }
 
     #[test]
     fn verify_answer() {
-        assert_eq!((super::make().solve)(), 0)
+        assert_eq!((super::make().solve)(), 1533776805)
     }
 }
-*/
